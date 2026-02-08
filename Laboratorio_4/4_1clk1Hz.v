ac038fb05be2b9af8e2bf7ef41dd083d36c2dfa3
Miguel Angel Perez Solano <138253801+maps58@users.noreply.github.com>
2026-02-08 14:38:57 -0600

    Add files via upload

---
+++ Laboratorio_4/4_1clk1Hz.v
@@ -0,0 +1,23 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Miguel Angel Pérez Solano
+// Instituto Tecnologico de Oaxaca
+// Departamento de Ingeniería Electrónica y Electrónica
+// Design Name: Circuitos Secuenciales
+// Module Name: clk1Hz
+
+module clkdiv (
+    input clk100MHz,
+    input rst,
+    output clk_out
+);
+    reg [25:0] q;
+
+    always @(posedge clk100MHz) begin
+        if (rst)
+            q <= 26'd0;
+        else
+            q <= q + 1'b1;
+    end
+    assign clk_out = q[25];   
+endmodule
---
+++ Laboratorio_4/4_3clk1_5Hz.v
@@ -0,0 +1,24 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Miguel Angel Pérez Solano
+// Instituto Tecnologico de Oaxaca
+// Departamento de Ingeniería Electrónica y Electrónica
+// Design Name: Circuitos Secuenciales
+// Module Name: clk10kHz
+
+
+module clk1_5Hz (
+    input  clk100MHz,
+    input  rst,   
+    output clk_out
+);
+
+    reg [25:0] q;
+    always @(posedge clk100MHz) begin
+        if (rst)
+            q <= 26'd0;
+        else
+            q <= q + 1'b1;
+    end
+    assign clk_out = q[25];   
+endmodule
