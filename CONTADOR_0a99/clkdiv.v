f3638790232a65b0fe78e8382b259ca98df4dc00
Miguel Angel Perez Solano <138253801+maps58@users.noreply.github.com>
2026-03-24 19:46:06 -0600

    Update clkdiv.v

--- CONTADOR_0a99/clkdiv.v
+++ CONTADOR_0a99/clkdiv.v
@@ -1,8 +1,12 @@
-`timescale 1ns / 1ps
+// Miguel Angel Pérez Solano
+// Instituto Tecnologico de Oaxaca
+// Departamento de Ingeniería Electrónica y Electrónica
+// Design Name: Circuitos Secuenciales
+// Module Name: clk1.4Hz
 
 module clkdiv(
     input wire clk,
-    output wire clk1Hz,
+    output wire clk1_4Hz,
     output wire clk190Hz
     );
     
@@ -12,7 +16,7 @@ module clkdiv(
   begin
             q <= q+1;
    end      
-   assign clk1Hz = q[25];     
+   assign clk1_4Hz = q[25];     
    assign clk190Hz = q[18];     
 
 endmodule
